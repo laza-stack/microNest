d7b99bae6208e9b25128d2b16b3573768b88e4a5
Lj <lazadiah@gmail.com>
2026-05-30 13:05:25 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,44 @@
-const CACHE_NAME = "micronest-v1";
+const CACHE_NAME = "micronest-v2";
 
 const urlsToCache = [
-  "/",
-  "/index.html",
-  "/style.css",
-  "/app.js",
-  "/firebase-config.js",
-  "/manifest.json",
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./firebase-config.js",
+  "./manifest.json"
 ];
 
-self.addEventListener("install", event => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-    .then(cache => cache.addAll(urlsToCache))
+      .then((cache) => cache.addAll(urlsToCache))
   );
+
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      )
+    )
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request)
-    .then(response => response || fetch(event.request))
+      .then((response) => {
+        return response || fetch(event.request);
+      })
   );
 });
